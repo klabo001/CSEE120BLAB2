309b8544e0d99827e0d980fa67df472579ab18a1
Kathleen Labog <klabo001@wch136-02.cs.ucr.edu>
2019-10-04 21:36:16 -0700

    fixed submission mistakes for part 2 and 3

--- turnin/klabo001_lab2_part3.c
+++ turnin/klabo001_lab2_part3.c
@@ -15,27 +15,24 @@
 int main(void) {
  /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's pins as outputs Initialize to 0x00
-    unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-    unsigned char tmpA0 = 0x00; // Temporary variable to hold the value of A
-    unsigned char tmpA1 = 0x00;
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's pins as outputs Initialize to 0x00
+    unsigned char cntavail = 0;
+    unsigned char parked = 0;
+    unsigned char spaceavail = 4;
+    
     while (1) {
 		//1) Read Input
-		tmpA0 = PINA & 0x01; // mask to check rightmost bit 
-		tmpA1 = PINA & 0x02; //mask to check second to the rightmost bit
-		
+		parked = (PINA & 0x01) + ((PINA>>1) & 0x01)+ ((PINA>>2) & 0x01)+((PINA>>3) & 0x01);overed = (PINA & 0x01) + ((PINA>>1) & 0x01)+ ((PINA>>2) & 0x01)+((PINA>>3) & 0x01);
 		//2) Perform Computation	
-		if (tmpA0 == 0x01 && tmpA1 == 0x00) //Checks if A1A0 = 01
-		{
-			tmpB = (tmpB & 0xFC) | 0x01; // sets tmpB to bbbbbb01
-										// clear rightmost 2 bits, then set to 01
-		}
-		else 
-		{
-			tmpB = (tmpB & 0xFC) | 0x00; // sets tmpB to bbbbbb00
-		}
+		cntavail = spaceavail - parked;
 		//3) Write Output
-		PORTB = tmpB;
+		
+		if (cntavail==0 )
+		{
+			cntavail = cntavail | 0x80; 
+		}
+		
+		PORTC = cntavail;
     }
     return 0;
 }
